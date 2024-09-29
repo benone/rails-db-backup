ee9cf7e81658df7e3b0ff6b05e670a39a1acd099
Kirill <hi.sender@gmail.com>
2024-09-29 12:53:59 +0200

    pg restore

--- lib/tools/postgres_adapter.rb
+++ lib/tools/postgres_adapter.rb
@@ -11,8 +11,14 @@ module Tools
     def restore(file_name, debug: false)
       file_path = File.join(backup_folder, file_name)
       output_redirection = debug ? '': ' > /dev/null'
-      cmd = "PGPASSWORD='#{password}' psql -U '#{user}' -h '#{host}' -d '#{database}' -f '#{file_path}' -p '#{port}' --clean --create #{output_redirection}"
+
+      drop_schema_cmd = "PGPASSWORD='#{password}' psql -U '#{user}' -h '#{host}' -d '#{database}' -p '#{port}' -c 'DROP SCHEMA public CASCADE; CREATE SCHEMA public;' > /dev/null 2>&1"
+      system(drop_schema_cmd)
+
+      # cmd = "PGPASSWORD='#{password}' psql -U '#{user}' -h '#{host}' -d '#{database}' -f '#{file_path}' -p '#{port}' --clean --create #{output_redirection}"
+      cmd = "PGPASSWORD='#{password}' psql -U '#{user}' -h '#{host}' -d '#{database}' -p '#{port}' -f #{file_path} #{output_redirection}"
       system(cmd)
+
       file_path
     end
 
